c7d23bc896143683b8280aa4462127bc5f184bfe
Noah Reeder <noahreederatc@gmail.com>
2025-04-13 02:18:04 -0500

    Fixed incorrect Boost component dependencies

--- config.in.cmake
+++ config.in.cmake
@@ -1,5 +1,6 @@
+include(CMakeFindDependencyMacro)
 # Copy any dependencies here
-find_dependency(Boost REQUIRED COMPONENTS thread)
+find_dependency(Boost REQUIRED COMPONENTS log log_setup)
 find_dependency(openFrameworksArduino CONFIG REQUIRED)
 
 include(${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@Targets.cmake)
