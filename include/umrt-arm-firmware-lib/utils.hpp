74d6607c2b634493687211c8cfa1de21b54c1b1a
Noah Reeder <noahreederatc@gmail.com>
2025-04-23 15:46:21 -0500

    Added big-endian packing utility functions

--- include/umrt-arm-firmware-lib/utils.hpp
+++ include/umrt-arm-firmware-lib/utils.hpp
@@ -33,6 +33,28 @@ inline std::vector<uint8_t> pack_32(const uint32_t integer) {
 }
 
 /**
+ * Packs a 32-bit integer into a vector of 8-bit integers, in big-endian format.
+ *
+ * E.g. for `0xDEAD_BEEF`:
+ * ```
+ * input:        1101 1110 1010 1101 1011 1110 1110 1111
+ * bit grouping: 0000 0000 1111 1111 2222 2222 3333 3333
+ * output:       { 0xDE, 0xAD, 0xBE, 0xEF }
+ * ```
+ *
+ * @param integer the integer to pack
+ * @return the byte representation of `integer`
+ */
+inline std::vector<uint8_t> pack_32_big(const uint32_t integer) {
+    return {
+        static_cast<uint8_t>(integer >> 24 & 0xFF), // bits [31, 24]
+        static_cast<uint8_t>(integer >> 16 & 0xFF), // bits [23, 16]
+        static_cast<uint8_t>(integer >> 8 & 0xFF),  // bits [15, 8]
+        static_cast<uint8_t>(integer & 0xFF)        // bits [7, 0]
+    };
+}
+
+/**
  * Packs a 16-bit integer into a vector of 8-bit integers, in little-endian format.
  *
  * E.g. for `0xBEEF`:
@@ -49,6 +71,26 @@ inline std::vector<uint8_t> pack_16(const uint16_t integer) {
     return {
         static_cast<uint8_t>(integer & 0xFF),     // bits [7, 0]
         static_cast<uint8_t>(integer >> 8 & 0xFF) // bits [15, 8]
+    };
+}
+
+/**
+ * Packs a 16-bit integer into a vector of 8-bit integers, in big-endian format.
+ *
+ * E.g. for `0xBEEF`:
+ * ```
+ * input:        1011 1110 1110 1111
+ * bit grouping: 0000 0000 1111 1111
+ * output:       { 0xBE, 0xEF }
+ * ```
+ *
+ * @param integer the integer to pack
+ * @return the byte representation of `integer`
+ */
+inline std::vector<uint8_t> pack_16_big(const uint16_t integer) {
+    return {
+        static_cast<uint8_t>(integer >> 8 & 0xFF), // bits [15, 8]
+        static_cast<uint8_t>(integer & 0xFF)       // bits [7, 0]
     };
 }
 
