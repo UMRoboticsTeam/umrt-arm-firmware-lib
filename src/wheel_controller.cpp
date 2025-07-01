48ef8d58c9df8c8fb86e51fe1502b265244423cb
Niko Christie <nikolaaschristie@gmail.com>
2025-07-01 12:42:35 -0500

    Start work on wheel controller

---
+++ src/wheel_controller.cpp
@@ -0,0 +1,190 @@
+//
+// Created by Niko Christie on 2025-07-01.
+//
+
+#include <boost/log/trivial.hpp>
+
+#include <ros2_socketcan/socket_can_receiver.hpp>
+#include <ros2_socketcan/socket_can_sender.hpp>
+
+#include <numeric>
+
+#include "MKS_COMMANDS.hpp"
+#include "wheel_controller.hpp"
+#include "utils.hpp"
+#include <cmath>
+
+uint8_t checksum(uint16_t driver_id, const std::vector<uint8_t>& payload);
+void packSpeedProperties(std::vector<uint8_t>& payload, const uint8_t acceleration, const int16_t normalised_speed, const bool dir);
+
+WheelController::MksStepperController(
+        const std::string& can_interface, std::shared_ptr<const std::unordered_set<uint16_t>> motor_ids,
+        const uint8_t norm_factor
+)
+    : motor_ids{ std::move(motor_ids) }, norm_factor{ norm_factor } {
+    BOOST_LOG_TRIVIAL(trace) << "MksStepperController construction begun";
+
+    this->can_receiver = std::make_unique<drivers::socketcan::SocketCanReceiver>(can_interface);
+    this->can_sender = std::make_unique<drivers::socketcan::SocketCanSender>(can_interface);
+
+    //TODO: Write norm_factor as microstepping factor to the driver
+
+    BOOST_LOG_TRIVIAL(debug) << "MksStepperController constructed";
+
+    setup_completed = true;
+}
+
+WheelController::~MksStepperController() noexcept { BOOST_LOG_TRIVIAL(debug) << "MksStepperController destructed"; }
+
+bool WheelController::setSpeed(const uint16_t motor, const int16_t speed, const uint8_t acceleration) {
+    if (!isSetup()) { return false; }
+
+    // Speed is normalised when norm_factor is 16
+    // That is, at 16 normalised_speed = speed
+    // At 1, normalised_speed = speed / 16
+    // At 32, normalised_speed = speed * 2
+    auto normalised_speed = static_cast<int16_t>(std::abs(speed) * (int32_t)16 / norm_factor);
+
+    std::vector<uint8_t> payload{ MksCommands::SET_SPEED };
+
+    packSpeedProperties(payload, acceleration, normalised_speed, speed > 0);
+
+    payload.insert(payload.end(), checksum(motor, payload));
+
+    try {
+        drivers::socketcan::CanId can_id(motor, 0, drivers::socketcan::FrameType::DATA, drivers::socketcan::StandardFrame);
+        can_sender->send(payload.data(), payload.size(), can_id);
+    } catch (drivers::socketcan::SocketCanTimeout& e) {
+        // Won't bother with e.what(), it is always "CAN Send timeout"
+        BOOST_LOG_TRIVIAL(warning) << "MksStepperController setSpeed timeout: motor=0x" << std::hex << motor << std::dec
+                                   << ", speed=" << normalised_speed << ", accel=" << acceleration;
+        return false;
+    }
+    return true;
+}
+
+// Included for posterity; due to strange responses this command is assumed to return the encoder speed, and we are not
+// using the driver's encoder so I cannot test this and it is not useful functionality anyways
+//bool WheelController::getSpeed(const uint16_t motor) {
+//    if (!isSetup()) { return false; }
+//
+//    std::vector<uint8_t> payload{ MksCommands::MOTOR_SPEED };
+//    payload.insert(payload.end(), checksum(motor, payload));
+//
+//    try {
+//        drivers::socketcan::CanId can_id(motor, 0, drivers::socketcan::FrameType::DATA, drivers::socketcan::StandardFrame);
+//        can_sender->send(payload.data(), payload.size(), can_id);
+//    } catch (drivers::socketcan::SocketCanTimeout& e) {
+//        BOOST_LOG_TRIVIAL(warning) << "MksStepperController getSpeed timeout: motor=" << motor;
+//        return false;
+//    }
+//
+//    return true;
+//}
+
+
+bool WheelController::getPosition(const uint16_t motor) {
+    if (!isSetup()) { return false; }
+
+    std::vector<uint8_t> payload{ MksCommands::CURRENT_POS };
+    payload.insert(payload.end(), checksum(motor, payload));
+
+    try {
+        drivers::socketcan::CanId can_id(motor, 0, drivers::socketcan::FrameType::DATA, drivers::socketcan::StandardFrame);
+        can_sender->send(payload.data(), payload.size(), can_id);
+    } catch (drivers::socketcan::SocketCanTimeout& e) {
+        BOOST_LOG_TRIVIAL(warning) << "MksStepperController getPosition timeout: motor=0x" << std::hex << motor << std::dec;
+        return false;
+    }
+    return true;
+}
+
+bool WheelController::isSetup() const { return this->setup_completed; }
+
+void WheelController::update(const std::chrono::nanoseconds& timeout) {
+    // Read a message from the CAN bus
+    // TODO: Consider bus-level message filtering for efficiency
+    try {
+        uint8_t msg_buffer[8];
+        drivers::socketcan::CanId msg_info = this->can_receiver->receive(&msg_buffer, timeout);
+
+        // If this isn't a standard CAN message, then it isn't a message applicable to us
+        if (msg_info.frame_type() != drivers::socketcan::FrameType::DATA) { return; }
+
+        // Turn the raw buffer into a vector
+        std::vector msg(msg_buffer, msg_buffer + msg_info.length());
+
+        this->handleCanMessage(msg, msg_info);
+    }
+    catch (drivers::socketcan::SocketCanTimeout& _) {} // Don't care if we don't receive a message
+}
+
+
+void WheelController::handleESetSpeed(const std::vector<uint8_t>& message, drivers::socketcan::CanId& info) {
+    if (message.size() != 3) { return; } // Don't want to process loop-backed requests, only responses
+    const auto status = static_cast<MksMoveResponse>(message.at(1));
+    BOOST_LOG_TRIVIAL(debug) << "[" << info.get_bus_time() << "]: SetSpeed received for motor 0x" << std::hex
+                             << info.identifier() << std::dec << " with status=" << to_string_mks_move_response(status);
+    ESetSpeed(static_cast<uint16_t>(info.identifier()), status == 1);
+}
+
+void WheelController::handleEGetPosition(const std::vector<uint8_t>& message, drivers::socketcan::CanId& info) {
+    if (message.size() != 6) { return; } // Don't want to process loop-backed requests, only responses
+    auto position = static_cast<int32_t>(decode_32_big(message.cbegin() + 1));
+    BOOST_LOG_TRIVIAL(debug) << "[" << info.get_bus_time() << "]: GetPosition received for motor 0x" << std::hex
+                             << info.identifier() << std::dec << " with position=" << position
+                             << ", normalised_position=" << position / norm_factor;
+    EGetPosition(static_cast<uint16_t>(info.identifier()), position / norm_factor);
+}
+
+void WheelController::handleCanMessage(const std::vector<uint8_t>& message, drivers::socketcan::CanId& info) {
+    // Note: info can't be const because get_bus_time isn't const-qualified...
+
+    // Drop message if not addressed to us
+    if (info.is_extended() || !motor_ids->count(static_cast<uint16_t>(info.identifier()))) {
+        // We are subscribing to all messages on the bus, there is no reason to spam our log over it
+        return;
+    }
+
+    // So this message is from a motor driver; it must contain a command or there is something weird happening
+    if (message.empty()) {
+        BOOST_LOG_TRIVIAL(error) << "[" << info.get_bus_time() << "]: Message received for motor 0x" << std::hex
+                                 << info.identifier() << std::dec << " with no payload";
+    }
+
+    // Process the message
+    switch (message[0]) {
+        case MksCommands::SET_SPEED: this->handleESetSpeed(message, info); break;
+        case MksCommands::CURRENT_POS: this->handleEGetPosition(message, info); break;
+        default:
+            // Again, we are subscribing to all messages on the bus, no need to spam log with ignored messages
+            break;
+    }
+}
+
+/**
+ * Calculates the "CRC" for an MKS message in accordance with @ref MksCommands.
+ * @param driver_id CAN ID of the driver this message is sent to
+ * @param payload CAN message payload
+ * @return computed checksum for the CAN message
+ */
+uint8_t checksum(uint16_t driver_id, const std::vector<uint8_t>& payload) {
+    // Note: Accumulate is going to work in uint8_t, and unsigned integer overflow is well-defined - no need for explicit modulo
+    return std::accumulate(payload.cbegin(), payload.cend(), static_cast<uint8_t>(driver_id));
+}
+
+/**
+ * Creates the speed properties structure used in the @ref MksCommands::SET_SPEED and @ref MksCommands::SEEK_POS commands.
+ * @param payload std::vector<uint8_t> to append the properties structure to
+ * @param acceleration the speed ramp profile, see @ref MksTest.Constants.MAX_ACCEL;
+ * @param normalised_speed speed value to write to the motor controller
+ * @param dir direction to spin, set to `true` if speed is positive
+ */
+void packSpeedProperties(std::vector<uint8_t>& payload, const uint8_t acceleration, const int16_t normalised_speed, const bool dir) {
+    const auto speed_properties_low =
+            static_cast<uint8_t>((normalised_speed & 0xF00) >> 8 | (dir ? 1u << 7 : 0u));
+    const auto speed_properties_high = static_cast<uint8_t>(normalised_speed & 0xFF);
+    payload.insert(payload.end(), speed_properties_low);
+    payload.insert(payload.end(), speed_properties_high);
+    payload.insert(payload.end(), acceleration);
+}
